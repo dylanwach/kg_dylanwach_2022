1d5d456e16951cf7a4ad62c33507222959a5954e
dylanwach <dylanwach@gmail.com>
2020-03-19 13:57:01 -0400

    Got arrays of indexes of same characters in s1. Next step is to make sure these indexes have same characters after mapping as well.

--- main.py
+++ main.py
@@ -17,3 +17,23 @@ if len(s1) != len(s2):
     print("false")
     quit()
 
+# array holding arrays of duplicate chars indexes that need to be mapped to same thing.
+duplicate_indexes = []
+s1_copy = s1
+for i in range(0, len(s1_copy)):
+    char = s1_copy[i]
+    # search for instances other than initial and store them in
+    duplicates = []
+    # copy is false until copy found, then becomes true, indicating that the duplicates indexes should be documented
+    copy = False
+    duplicates.append(i)
+    for j in range(i+1, len(s1_copy)):
+        # find where the char is found in copied string, and it hasn't already been removed (documented)
+        if s1_copy[j] == char and s1.find(char) != -1:
+            duplicates.append(j)
+            copy = True
+    if copy:
+        duplicate_indexes.append(duplicates)
+    s1 = s1.replace(char, '')
+
+print(duplicate_indexes)
